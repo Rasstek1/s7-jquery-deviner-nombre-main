6d7c8d897460ced4dc57c0288f200906cf305afe
RassTek78 <2295093@cegepoutaouais.qc.ca>
2023-02-22 14:25:44 -0500

    mercredi

--- script.js
+++ script.js
@@ -98,3 +98,31 @@ resetButton.addEventListener("click", function () {
     cell.textContent = "";
   });
 });
+
+
+//code Maryse
+/*let aleatoire = Math.ceil(Math.random()*100); //le nombre à deviner
+let aleatoire = Math.ceil(Math.random()*100); //le nombre à deviner
+
+$("button").click(function (){
+    let $nb = $("#essai").val();
+    if (+$nb === aleatoire){
+        $("#messages").text("Bravo");
+    }
+    else if(+$nb > aleatoire){
+        $("#messages").text("Trop grand");
+        effacerNombres(+$nb, 100);
+        $("#essais").append($nb + " ");
+    }
+    else {
+        $("#messages").text("Trop petit");
+        effacerNombres(0, +$nb);
+        $("#essais").append($nb + " ");
+    }
+});
+
+function effacerNombres(min, max){
+    for (let i = min; i <= max; i++){
+        $("#"+i).hide();
+    }
+}*/
